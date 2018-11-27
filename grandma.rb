c63ddd56be668d58bd041d00ca7fdd239fecdb47
Don Ward <10is0420@gmail.com>
2018-11-27 00:15:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-speak_to_grandma(prompt)
+def speak_to_grandma(prompt)
 if prompt == "I LOVE YOU GRANDMA"
   puts "I LOVE YOU TOO PUMPKIN"
 elsif prompt == "Hi Nana, how are you"
