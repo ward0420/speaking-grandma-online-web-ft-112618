6aa5c919f394afba31adba3c4272fd9203061983
Don Ward <10is0420@gmail.com>
2018-11-27 00:18:31 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,22 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(prompt)
+if prompt == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
+elsif prompt == "Hi Nana, how are you?"
+  puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
+elsif prompt == "Hi!"
+  puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
+elsif prompt == "WHAT DID YOU EAT TODAY?"
+  puts "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
+else prompt == "WHAT?"
+  puts  "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
+  end
+end
